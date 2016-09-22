5cd5273757454457fd4fae4f12983a3f99840562
emma <emmaruthoneill@gmail.com>
2016-09-22 09:27:57 -0700

    made a lot of fixes

--- src/js/main.js
+++ src/js/main.js
@@ -9,8 +9,23 @@ function activate() {
   for (var i = 1986; i < 2017; i++ ) {
   	var a = document.getElementById(i);
   	var at = document.getElementById(i);
-    var ed_top = a.getBoundingClientRect().top + window_top - 100;
-  	var ede_top = a.getBoundingClientRect().bottom + window_top - 100;
+    if (screen.width > 480) {
+      if (i == 2016) {
+        var ed_top = a.getBoundingClientRect().top + window_top-100;
+      	var ede_top = a.getBoundingClientRect().bottom + window_top-200;
+      } else {
+        var ed_top = a.getBoundingClientRect().top + window_top - 100;
+      	var ede_top = a.getBoundingClientRect().bottom + window_top - 100;
+      }
+    } else {
+      if (i == 2016) {
+        var ed_top = a.getBoundingClientRect().top + window_top-20;
+        var ede_top = a.getBoundingClientRect().bottom + window_top-200;
+      } else {
+        var ed_top = a.getBoundingClientRect().top + window_top-20;
+        var ede_top = a.getBoundingClientRect().bottom + window_top - 100;
+      }
+    }
 
     if (window_top > ed_top && window_top < ede_top) {
 	        at.classList.add('active');
